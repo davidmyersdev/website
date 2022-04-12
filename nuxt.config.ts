0d023a572dba2097d1a8e9beba2f933a12bbe1c2
David R. Myers <davidrmyersii@gmail.com>
2022-04-11 23:29:35 -0400

    Add Nuxt3/Vitesse boilerplate

---
+++ nuxt.config.ts
@@ -0,0 +1,34 @@
+import { defineNuxtConfig } from 'nuxt3'
+
+export default defineNuxtConfig({
+  meta: {
+    title: 'David Myers | voracious.dev',
+    link: [
+      {
+        rel: 'icon', type: 'image/png', href: '/nuxt.png',
+      },
+    ],
+  },
+  buildModules: [
+    '@vueuse/nuxt',
+    '@unocss/nuxt',
+    '@pinia/nuxt',
+  ],
+  vueuse: {
+    ssrHandlers: true,
+  },
+  unocss: {
+    uno: true,
+    attributify: true,
+    preflight: true,
+    icons: {
+      scale: 1.2,
+    },
+    shortcuts: [
+      ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
+    ],
+  },
+  // experimental: {
+  //   viteNode: true,
+  // },
+})
---
+++ pages/[slug].vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const route = useRoute()
+const slug = route.params.slug
+</script>
+
+<template>
+  <div>
+    <div text-xl>
+      {{ slug }}
+    </div>
+
+    <div>
+      <NuxtLink
+        class="btn m-3 text-sm"
+        to="/"
+      >
+        Back
+      </NuxtLink>
+    </div>
+  </div>
+</template>
