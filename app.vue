325c80dad0a814ae1f0e8b158c1cecc903dd7094
David R. Myers <davidrmyersii@gmail.com>
2022-04-17 00:12:26 -0400

    Use a real link to home page for now

--- app.vue
+++ app.vue
@@ -9,7 +9,7 @@ const toggleMenu = () => {
   <nav class="flex flex-col justify-between bg-darkest mb-12 gap-4 lg:mb-20 lg:gap-8 lg:flex-row">
     <div class="flex items-center justify-between">
       <div class="flex items-center">
-        <NuxtLink class="p-4" to="/">David R. Myers</NuxtLink>
+        <a class="p-4" href="/">David R. Myers</a>
       </div>
       <button class="flex items-center p-4 lg:hidden" @click="toggleMenu">
         <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
