31b9396401c1518e7ce4cae5e23dabf3b6078526
David R. Myers <davidrmyersii@gmail.com>
2022-07-05 16:23:29 -0400

    Use OG images on TIL pages

--- pages/til/[slug].vue
+++ pages/til/[slug].vue
@@ -1,6 +1,16 @@
 <script lang="ts" setup>
 const slug = useRoute().params.slug
 const path = `/til/${slug}`
+
+useHead({
+  meta: [
+    {
+      hid: 'og:image',
+      property: 'og:image',
+      content: `/og/content/${slug}.png`,
+    },
+  ],
+})
 </script>
 
 <template>
