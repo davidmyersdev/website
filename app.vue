bb91004c713439372cf8eb3feba8de3914bd8238
David R. Myers <davidrmyersii@gmail.com>
2022-07-11 20:40:33 -0400

    Add 'Support Ukraine' link

--- app.vue
+++ app.vue
@@ -6,7 +6,7 @@ const toggleMenu = () => {
 </script>
 
 <template>
-  <nav class="flex flex-col justify-between bg-darkest mb-12 gap-4 lg:mb-20 lg:gap-8 lg:flex-row">
+  <nav class="flex flex-col justify-between bg-darkest mb-12 gap-2 lg:mb-20 lg:gap-8 lg:flex-row">
     <div class="flex items-center justify-between">
       <div class="flex items-center">
         <NuxtLink class="p-4" to="/">The Voracious Developer</NuxtLink>
@@ -17,11 +17,12 @@ const toggleMenu = () => {
         </svg>
       </button>
     </div>
-    <div :class="{ 'hidden': !showMenu }" class="flex flex-col items-center lg:flex lg:flex-row">
+    <div :class="{ 'hidden': !showMenu }" class="flex flex-col items-center pb-4 lg:flex lg:flex-row lg:pb-0">
       <a class="p-4" href="https://twitter.com/voraciousdev" rel="noopener" target="_blank">Twitter</a>
       <a class="p-4" href="https://github.com/voraciousdev" rel="noopener" target="_blank">GitHub</a>
       <a class="p-4" href="https://octo.app" rel="noopener" target="_blank">Octo</a>
       <NuxtLink class="p-4" to="/sheets">Cheat Sheets</NuxtLink>
+      <a class="p-4 text-[#fcdf51]" href="https://war.ukraine.ua/support-ukraine/" rel="noopener" target="_blank">Support Ukraine</a>
     </div>
   </nav>
   <main class="px-5 pb-24 text-sm max-w-[75ch] w-full mx-auto lg:text-base">
