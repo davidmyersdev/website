4c8eed7c671f581f73183021a91a8463f88be8ce
David R. Myers <davidrmyersii@gmail.com>
2023-01-30 12:08:50 -0500

    Add a custom link component

---
+++ components/content/ProseA.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const props = defineProps<{ href?: string, target?: string }>()
+const isExternal = computed(() => /^(\w+\:)?\/\//.test(`${props.href}`))
+const target = computed(() => isExternal.value ? '_blank' : props.target)
+</script>
+
+<template>
+  <a v-if="isExternal" :href="href" :target="target"><slot /></a>
+  <NuxtLink v-else :href="href" :target="target"><slot /></NuxtLink>
+</template>
