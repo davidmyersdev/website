54bb500dfb3b4c49144ef4c895fd57d29127ea5c
David R. Myers <davidrmyersii@gmail.com>
2023-02-03 10:49:29 -0500

    Set canonical for homepage to blog

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,13 @@
+<script lang="ts" setup>
+const { public: { baseUrl }} = useRuntimeConfig()
+
+useHead(() => ({
+  link: [
+    { rel: 'canonical', href: `${baseUrl}/blog` },
+  ],
+}))
+</script>
+
 <template>
   <PostPreviewList path="/" />
 </template>
